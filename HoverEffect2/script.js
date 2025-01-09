c95937d1885fafc9d27c627329f4f4eadbe983a5
Avinash Sharma <Avinashsharma31384@gmail.com>
2025-01-09 12:47:43 +0530

    After some fixing again

--- HoverEffect2/script.js
+++ HoverEffect2/script.js
@@ -1,64 +1,100 @@
-let container=document.querySelector(".container")
+// let container=document.querySelector(".container")
 
-console.log(window.innerWidth)
-console.log(window.innerHeight)
+// console.log(window.innerWidth)
+// console.log(window.innerHeight)
 
+
+// const colors = [
+//     "#FFFFFF", // White
+//     "#000000", // Black
+//     "#FF0000", // Red
+//     "#00FF00", // Green
+//     "#0000FF", // Blue
+//     "#FFFF00", // Yellow
+//     "#00FFFF", // Cyan
+//     "#FF00FF", // Magenta
+//     "#808080", // Gray
+//     "#D3D3D3", // Light Gray
+//     "#A9A9A9", // Dark Gray
+//     "#FFA500", // Orange
+//     "#FFC0CB", // Pink
+//     "#800080", // Purple
+//     "#A52A2A", // Brown
+//     "#8B0000", // Dark Red
+//     "#FFD700", // Gold
+//     "#ADFF2F", // Green Yellow
+//     "#4B0082", // Indigo
+//     "#F08080", // Light Coral
+//     "#20B2AA", // Light Sea Green
+//     "#87CEFA", // Light Sky Blue
+//     "#778899", // Light Slate Gray
+//     "#B0C4DE"  // Light Steel Blue
+// ];
+
+// let randomC=()=>{
+//     return colors[Math.floor(Math.random()*colors.length)]
+// }
+// // console.log(randomC())
+
+
+
+// let setColor=(element)=>{
+//     element.style.backgroundColor=randomC()
+// }
+
+
+// let unsetColor=(element)=>{
+//     element.style.backgroundColor="rgb(32, 32, 32)"
+// }
+
+
+
+// for(let i=0; i<=8950; i++){
+//     let squre=document.createElement("div")
+//     squre.classList.add("squre")
+//     container.appendChild(squre)
+
+//     squre.addEventListener("mouseover",()=>{
+//         setColor(squre)
+//     })
+
+//     squre.addEventListener("mouseout",()=>{
+//         unsetColor(squre)
+//     })
+// }
+
+
+let container = document.querySelector(".container");
 
 const colors = [
-    "#FFFFFF", // White
-    "#000000", // Black
-    "#FF0000", // Red
-    "#00FF00", // Green
-    "#0000FF", // Blue
-    "#FFFF00", // Yellow
-    "#00FFFF", // Cyan
-    "#FF00FF", // Magenta
-    "#808080", // Gray
-    "#D3D3D3", // Light Gray
-    "#A9A9A9", // Dark Gray
-    "#FFA500", // Orange
-    "#FFC0CB", // Pink
-    "#800080", // Purple
-    "#A52A2A", // Brown
-    "#8B0000", // Dark Red
-    "#FFD700", // Gold
-    "#ADFF2F", // Green Yellow
-    "#4B0082", // Indigo
-    "#F08080", // Light Coral
-    "#20B2AA", // Light Sea Green
-    "#87CEFA", // Light Sky Blue
-    "#778899", // Light Slate Gray
-    "#B0C4DE"  // Light Steel Blue
+    "#FFFFFF", "#000000", "#FF0000", "#00FF00", "#0000FF", "#FFFF00", "#00FFFF",
+    "#FF00FF", "#808080", "#D3D3D3", "#A9A9A9", "#FFA500", "#FFC0CB", "#800080",
+    "#A52A2A", "#8B0000", "#FFD700", "#ADFF2F", "#4B0082", "#F08080", "#20B2AA",
+    "#87CEFA", "#778899", "#B0C4DE"
 ];
 
-let randomC=()=>{
-    return colors[Math.floor(Math.random()*colors.length)]
-}
-// console.log(randomC())
+let randomC = () => {
+    return colors[Math.floor(Math.random() * colors.length)];
+};
 
+let setColor = (element) => {
+    element.style.backgroundColor = randomC();
+};
 
+let unsetColor = (element) => {
+    element.style.backgroundColor = "rgb(32, 32, 32)";
+};
 
-let setColor=(element)=>{
-    element.style.backgroundColor=randomC()
-}
+for (let i = 0; i <= 8950; i++) {
+    let squre = document.createElement("div");
+    squre.classList.add("squre");
+    container.appendChild(squre);
 
+    squre.addEventListener("mouseover", () => {
+        setColor(squre);
+    });
 
-let unsetColor=(element)=>{
-    element.style.backgroundColor="rgb(32, 32, 32)"
-}
-
-
-
-for(let i=0; i<=8950; i++){
-    let squre=document.createElement("div")
-    squre.classList.add("squre")
-    container.appendChild(squre)
-
-    squre.addEventListener("mouseover",()=>{
-        setColor(squre)
-    })
-
-    squre.addEventListener("mouseout",()=>{
-        unsetColor(squre)
-    })
+    squre.addEventListener("mouseout", () => {
+        unsetColor(squre);
+    });
 }
--- color2/color.js
+++ color2/color.js
@@ -1,32 +1,74 @@
-let red=document.querySelector(".red")
-let green=document.querySelector(".green")
-let yellow=document.querySelector(".yellow")
-let blue=document.querySelector(".blue")
-let black=document.querySelector(".black")
-let orange=document.querySelector(".orange")
-let gold=document.querySelector(".gold")
-let prebtn=document.querySelector(".prebtn")
-let nextbtn=document.querySelector(".nextbtn")
+// let red=document.querySelector(".red")
+// let green=document.querySelector(".green")
+// let yellow=document.querySelector(".yellow")
+// let blue=document.querySelector(".blue")
+// let black=document.querySelector(".black")
+// let orange=document.querySelector(".orange")
+// let gold=document.querySelector(".gold")
+// let prebtn=document.querySelector(".prebtn")
+// let nextbtn=document.querySelector(".nextbtn")
 
 
-let count=0
+// let count=0
 
 
-nextbtn.addEventListener("click", ()=>{
-   
-    green.style.backgroundColor='black'
-    yellow.style.backgroundColor='green'
-    blue.style.backgroundColor='yellow'
-    black.style.backgroundColor='blue'
-    orange.style.backgroundColor='black'
-    gold.style.backgroundColor='orange'
-    orange.style.backgroundColor='red'
-    red.style.backgroundColor='gold'
-  
-})
+// nextbtn.addEventListener("click", ()=>{
+
+//     green.style.backgroundColor='black'
+//     yellow.style.backgroundColor='green'
+//     blue.style.backgroundColor='yellow'
+//     black.style.backgroundColor='blue'
+//     orange.style.backgroundColor='black'
+//     gold.style.backgroundColor='orange'
+//     orange.style.backgroundColor='red'
+//     red.style.backgroundColor='gold'
+
+// })
 
 
 
-prebtn.addEventListener('click',()=>{
-    red.style.backgroundColor='green'
-})
+// prebtn.addEventListener('click',()=>{
+//     red.style.backgroundColor='green'
+// })
+
+
+
+document.addEventListener("DOMContentLoaded", () => {
+    let red = document.querySelector(".red");
+    let green = document.querySelector(".green");
+    let yellow = document.querySelector(".yellow");
+    let blue = document.querySelector(".blue");
+    let black = document.querySelector(".black");
+    let orange = document.querySelector(".orange");
+    let gold = document.querySelector(".gold");
+    let prebtn = document.querySelector(".prebtn");
+    let nextbtn = document.querySelector(".nextbtn");
+
+    if (!prebtn || !nextbtn) {
+        console.error("Buttons not found");
+        return;
+    }
+
+    let colors = ["red", "green", "yellow", "blue", "black", "orange", "gold"];
+    let count = 0;
+
+    nextbtn.addEventListener("click", () => {
+        count = (count + 1) % colors.length;
+        updateColors();
+    });
+
+    prebtn.addEventListener("click", () => {
+        count = (count - 1 + colors.length) % colors.length;
+        updateColors();
+    });
+
+    function updateColors() {
+        red.style.backgroundColor = colors[(count + 0) % colors.length];
+        green.style.backgroundColor = colors[(count + 1) % colors.length];
+        yellow.style.backgroundColor = colors[(count + 2) % colors.length];
+        blue.style.backgroundColor = colors[(count + 3) % colors.length];
+        black.style.backgroundColor = colors[(count + 4) % colors.length];
+        orange.style.backgroundColor = colors[(count + 5) % colors.length];
+        gold.style.backgroundColor = colors[(count + 6) % colors.length];
+    }
+});
